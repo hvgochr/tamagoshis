b60d79ab460fb3d716592453a14162e0514c1f67
hvgochr <charrade.hugo@gmail.com>
2024-01-16 08:57:04 +0100

    Add App.java

---
+++ app/src/main/java/tamagoshis/App.java
@@ -0,0 +1,12 @@
+package tamagoshis;
+
+import tamagoshis.game.Game;
+
+public class App {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+
+}
